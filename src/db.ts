58cb5d8c8b6ba3c47c307f81f474e83555e436a7
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-03 11:17:07 +0000

    Modified databse config

--- src/db.ts
+++ src/db.ts
@@ -2,20 +2,20 @@ import { Client } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const myConnectionString =
-//   process.env.NODE_ENV === "production"
-//     ? process.env.DATABASE_URL
-//     : process.env.URL;
-
-// const client = new Client({
-//   connectionString: myConnectionString,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const myConnectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : process.env.URL;
 
 const client = new Client({
-  database: "ice-cream-shop",
+  connectionString: myConnectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// const client = new Client({
+//   database: "ice-cream-shop",
+// });
 
 export default client;
